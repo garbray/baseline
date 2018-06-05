21858323716d3aa993dbc33975170ce4f910f062
César Moreno <cmoreno@hugeinc.com>
2018-06-05 15:00:25 -0500

    Adding stylelint

--- build-scripts/postcss.config.js
+++ build-scripts/postcss.config.js
@@ -5,15 +5,10 @@ module.exports = () => ({
     // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
     // https://github.com/postcss/postcss-import
     require('postcss-import')(),
+    require("stylelint")(),
     require('postcss-cssnext')(),
     // Allows nested media queries and compresses all breakpoints into one media query per file
     // https://github.com/hail2u/node-css-mqpacker
     require('css-mqpacker')(),
-    // Add vendor prefixes to CSS rules using values from caniuse.com
-    // https://github.com/postcss/autoprefixer
-    require('autoprefixer')({
-      browsers: pkg.browserslist,
-      flexbox: 'no-2009',
-    }),
   ],
 });
