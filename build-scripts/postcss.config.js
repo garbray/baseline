5839bc36dcbb1fc9ee18b8b10b6f81b53b31e8c8
Bryan Garzon Bahamon <bgarzon@hugeinc.com>
2018-06-06 06:27:59 -0500

    adding more postcss plugins

--- build-scripts/postcss.config.js
+++ build-scripts/postcss.config.js
@@ -5,10 +5,30 @@ module.exports = () => ({
     // Transfer @import rule by inlining content, e.g. @import 'normalize.css'
     // https://github.com/postcss/postcss-import
     require('postcss-import')(),
-    require("stylelint")(),
+    require('stylelint')(),
     require('postcss-cssnext')(),
     // Allows nested media queries and compresses all breakpoints into one media query per file
     // https://github.com/hail2u/node-css-mqpacker
     require('css-mqpacker')(),
+    // W3C CSS Custom Media Queries, e.g. @custom-media --small-viewport (max-width: 30em);
+    // https://github.com/postcss/postcss-custom-media
+    require('postcss-custom-media')(),
+    // Allows you to nest one style rule inside another
+    // https://github.com/jonathantneal/postcss-nesting
+    require('postcss-nesting')(),
+    // Unwraps nested rules like how Sass does it
+    // https://github.com/postcss/postcss-nested
+    require('postcss-nested')(),
+    // Converts px to rems to allow easy global font adjustments
+    // https://github.com/cuth/postcss-pxtorem
+    require('postcss-pxtorem')({
+      rootValue: 16,
+      unitPrecision: 5,
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: true,
+      minPixelValue: 0,
+    }),
   ],
 });
